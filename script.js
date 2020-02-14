9dfd884eb5e2656a3a28902ae1c7c0a41f319173
taishiasami123 <taishi.asami123@gmail.com>
2020-02-14 17:37:11 +0900

    ユーザーログインAPIを実装

--- script.js
+++ script.js
@@ -34,9 +34,34 @@ const register = () =>{
         });
     }
 
+// ユーザーログイン
+const login = () =>{
+    const email = document.getElementById("signInInputEmail").value;
+    const password = document.getElementById("signInInputPwd").value;
+    const passwordConfirmation = document.getElementById("signInInputCfmPwd").value;
+    const data = {
+        sign_in_user_params: {
+            email: email,
+            password: password,
+            password_confirmation: passwordConfirmation
+        }
+    }
+    console.log("今送ったデータ");
+    console.log(JSON.stringify(data));
+    fetch("https://teachapi.herokuapp.com/sign_in", {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         headers: {
             "Content-Type": "application/json; charset=utf-8",
             // "Content-Type": "application/x-www-form-urlencoded",
         },
         body: JSON.stringify(data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+    })
+        .then(response => {
+            const json = response.json();
+            return json;
+        })
+        .then(json => {
+            console.log(json);
+        });
+    }
+
