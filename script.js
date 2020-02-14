f12a560b8bcbd5a323aa16dfa92ec4d7b648e48b
taishiasami123 <taishi.asami123@gmail.com>
2020-02-14 17:38:39 +0900

    API実装の雛形をコメントアウトして残す

--- script.js
+++ script.js
@@ -85,3 +85,43 @@ const userList = () =>{
 }
 
 userList();
+
+// const data = {
+//     sign_up_user_params: {
+//         name: name,
+//         bio: bio,
+//         email: email,
+//         password: password,
+//         password_confirmation: password_confirmation
+//     }
+// }
+
+// console.log("今送ったデータ")
+// console.log(JSON.stringify(data))
+
+// fetch("https://teachapi.herokuapp.com/sign_up", {
+//     method: "POST", // *GET, POST, PUT, DELETE, etc.
+//     headers: {
+//         "Content-Type": "application/json; charset=utf-8",
+//         // "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: JSON.stringify(data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+// })
+
+//     .then(response => {
+//         const json = response.json();
+//         return json;
+//     })
+
+//     .then(json => {
+//         console.log(json);
+//     });
+
+
+// return fetch(url, {
+//     method: "POST", // *GET, POST, PUT, DELETE, etc.
+//     headers: {
+//         "Content-Type": "application/json; charset=utf-8",
+//         // "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: JSON.stringify(data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
