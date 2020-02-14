b3c7f30c9ef131a04cc681d1d356dbf674679cf4
taishiasami123 <taishi.asami123@gmail.com>
2020-02-14 17:36:49 +0900

    ユーザー登録APIを実装

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 
+//ユーザー登録
+const register = () =>{
+    const name = document.getElementById("signUpInputName").value;
+    const bio = document.getElementById("signUpInputBio").value;
+    const email = document.getElementById("signUpInputEmail").value;
+    const password = document.getElementById("signUpInputPwd").value;
+    const password_confirmation = document.getElementById("signUpInputCfmPwd").value    
+    const data = {
+        sign_up_user_params: {
+            name: name,
+            bio: bio,
+            email: email,
+            password: password,
+            password_confirmation: password_confirmation
+        }
+    }
+    console.log("今送ったデータ");
+    console.log(JSON.stringify(data));
+    fetch("https://teachapi.herokuapp.com/sign_up", {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
     })
+        .then(response => {
+            const json = response.json();
+            return json;
+        })
+        .then(json => {
+            console.log(json);
+        });
+    }
 
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         headers: {
