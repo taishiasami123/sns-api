4d251f86ba02133134ee33123902a79276186c14
taishiasami123 <taishi.asami123@gmail.com>
2020-02-14 17:36:18 +0900

    fetchメソッドを削除(各APIに内包するため)

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
 
-fetch("https://teachapi.herokuapp.com/sign_up", {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        // "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: JSON.stringify(data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
-})
-
-    .then(response => {
-        const json = response.json();
-        return json;
     })
 
-    .then(json => {
-        console.log(json);
-    });
-
-/*
-    return fetch(url, {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         headers: {
             "Content-Type": "application/json; charset=utf-8",
             // "Content-Type": "application/x-www-form-urlencoded",
         },
         body: JSON.stringify(data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
-*/
